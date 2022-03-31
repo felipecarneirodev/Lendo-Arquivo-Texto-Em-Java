5519cd2fe494da2a5d34f2bb230bd727969ff715
felipecarneirodev <felipecarneirodev@gmail.com>
2022-03-30 21:02:19 -0300

    FileReader e BufferedReader com controle manual

--- LendoArquivoDeTexto/src/application/Program.java
+++ LendoArquivoDeTexto/src/application/Program.java
@@ -1,29 +1,39 @@
 package application;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		File file = new File("c:\\temp\\in.txt");
-		Scanner sc = null;		
+		String path = "C:\\temp\\in.txt";
+		
+		FileReader fr = null;
+		BufferedReader br = null;
+		
 		try {
-			sc = new Scanner(file);
-			while(sc.hasNextLine()) {
-				System.out.println(sc.nextLine());
+			fr = new FileReader(path);
+			br = new BufferedReader(fr);
+			
+			String line = br.readLine();
+			while(line != null) {
+				System.out.println(line);
+				line = br.readLine();
 			}
-		}
+		}	
 		catch(IOException e) {
-			System.out.println(e.getMessage());
+			System.out.println("Error: " + e.getMessage());
 		}
 		finally {
-			if(sc != null) {
-			sc.close();
+			try {
+			fr.close();
+			br.close();
+			}
+			catch(IOException e) {
+				e.printStackTrace();
 			}
 		}
 	}
-
 }
